c9d44a96d93be9fb9b789a37378372c2900bee39
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-03-24 12:44:34 -0300

    fix callout types

--- src/components/elements/Callout/Callout.types.ts
+++ src/components/elements/Callout/Callout.types.ts
@@ -3,7 +3,7 @@ import {
   IntentType, ITestableProps,
 } from '../../../types';
 
-export interface ICalloutProps extends ITestableProps, React.HtmlHTMLAttributes<HTMLElement> {
+export interface ICalloutProps extends ITestableProps, Omit<React.HtmlHTMLAttributes<HTMLDivElement>, 'title'> {
   icon?: React.ReactNode;
   intent?: IntentType;
   title?: React.ReactNode;
