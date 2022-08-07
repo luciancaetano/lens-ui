d271945cea74b9f4e095ec878c05f30ba90280c3
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-08-07 00:50:02 -0300

    fix(elements): :bug: fixed type of modal component

--- src/components/elements/Modal/index.ts
+++ src/components/elements/Modal/index.ts
@@ -1,11 +1,11 @@
-import React from 'react';
+import { CompoundedComponentWithRefType } from '../../../types';
 import Modal from './Modal/Modal';
 import { IModalProps } from './Modal/Modal.types';
 import ModalContent from './ModalContent/ModalContent';
 import ModalFooter from './ModalFooter/ModalFooter';
 import ModalHeader from './ModalHeader/ModalHeader';
 
-interface ICompoundedComponent extends React.ForwardRefExoticComponent<IModalProps> {
+interface ICompoundedComponent extends CompoundedComponentWithRefType<IModalProps, HTMLDivElement> {
   Header: typeof ModalHeader;
   Content: typeof ModalContent;
   Footer: typeof ModalFooter;
