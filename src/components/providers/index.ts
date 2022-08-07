fc9441c3ac086bb2f56cdd6f371a3f7867388b16
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-08-06 22:07:09 -0300

    refactor(providers): :fire: remove alert and application providers
    458 / 5.000 Resultados de tradução We rethought the useAlert hook and are changing its definition so that there is no need for a context, simplifying its use. Regarding the Application provider, due to the removal of the contexts consumed by the useAlert and useModal hooks, it is no longer necessary. In a general context we will be allowing the user to have more freedom in choosing their tools as well as the ease of enabling or disabling lens-ui features.

--- src/components/providers/index.ts
+++ src/components/providers/index.ts
@@ -1,6 +1,5 @@
-export { default as ApplicationProvider } from './ApplicationProvider/ApplicationProvider';
+export { default as DeviceProvider } from './DeviceProvider/DeviceProvider';
+export { default as ToastProvider } from './ToastProvider/ToastProvider';
 
 export * from './DeviceProvider/DeviceProvider.types';
-export * from './AlertProvider/AlertProvider.types';
-export * from './ApplicationProvider/ApplicationProvider.types';
 export * from './ToastProvider/ToastProvider.types';
