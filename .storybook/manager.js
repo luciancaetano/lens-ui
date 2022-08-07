9fbdd04263a0beb09650a439857130116e3e01ea
Lucian Caetano <lucian.santos.caetano@gmail.com>
2022-08-07 05:50:02 -0300

    docs: :memo: Writing docs

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,7 @@
 import { addons } from '@storybook/addons';
 import theme from './theme';
 import './titleAddon';
+import { addParameters } from '@storybook/react';
 
 addons.setConfig({
   theme,
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import anysort from 'anysort';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,5 +8,31 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  options: {
+    /**
+     * display the top-level grouping as a "root" in the sidebar
+     * @type {Boolean}
+     */
+    showRoots: true,
+    storySort: (previous, next) => {
+      const [previousStory, previousMeta] = previous
+      const [nextStory, nextMeta] = next
+
+      return anysort(previousMeta.kind, nextMeta.kind, [
+        'Overview/Introduction',
+        'Overview/Getting Started',
+        'General/**',
+        'Navigation/**',
+        'Layout/**',
+        'Data Entry/**',
+        'Data Display/**',
+        'Feedback/**',
+        'Icon/Introduction',
+        'Icon/**',
+        'Hooks/**',
+        'Examples/**',
+      ])
+    }
   },
 }
